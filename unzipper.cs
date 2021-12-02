3b368b62882b5abf7447a1441b22d73130d621b5
KenoBuss <56350765+KenoBuss@users.noreply.github.com>
2021-12-02 20:57:37 +0100

    Add files via upload

---
+++ unzipper.cs
@@ -0,0 +1,31 @@
+﻿using System.IO.Compression;
+
+namespace unzipper
+{
+    public class unzipper
+    {
+        public static void Main()
+        {
+            goDeeper();
+            Console.Clear();
+            Console.WriteLine("Das Programm ist nun fertig.");
+        }
+
+        public static void goDeeper()
+        {
+            //Enter here a path to a folder containing only the "Geschenk.zip" file e.g. "C:/user/user/desktop/new_folder/". !!!enter a '/' at the End!!! 
+            string filePath = "";
+            DirectoryInfo dir = new DirectoryInfo(filePath); //create a directory to or file
+
+            foreach (FileInfo fi in dir.GetFiles()) //foreach file in dir
+            {
+                if (fi.Name.Contains(".zip")) //if file contains ".zip"
+                {
+                    ZipFile.ExtractToDirectory(filePath + fi.Name, filePath); //ectract the file to or folder 
+                    File.Delete(filePath + fi.Name); //delete the old file (in case ther are Duplicate names)
+                    goDeeper(); //call goDeeper recusive
+                }
+            }
+        }
+    }
+}
